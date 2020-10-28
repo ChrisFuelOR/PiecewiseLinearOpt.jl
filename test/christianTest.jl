92b3a49e14a48786ff5b49b2b1004a1607208180
Christian Fuellner <christian.fuellner@kit.edu>
2020-10-28 13:41:43 +0100

    Some tests for using this package

--- test/christianTest.jl
+++ test/christianTest.jl
@@ -1,6 +1,7 @@
 using JuMP
 using PiecewiseLinearOpt
 
+
 m = Model()
 @variable(m, x)
 d = [0, 1, 2, 3, 4]
@@ -8,6 +9,6 @@ f(x) = x^2
 fd = [f(x) for x in d]
 println(all_variables(m))
 
-piecewiselinear(m, x, d, fd; method = :DLog)
+PiecewiseLinearOpt.piecewiselinear(m, x, d, fd; method = :DLog)
 
 println(all_variables(m))
